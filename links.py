6aaecdc95208f9be220e58fdccec2b5848f7274e
liziyu1100 <1194981710@qq.com>
2023-12-17 01:01:16 +0800

    click

--- links.py
+++ links.py
@@ -1,9 +1,10 @@
+import copy
 import ctypes
 from ctypes.wintypes import RECT, HWND
 import numpy as np
 import pyautogui
 # use 'pip install pywin32' to install
-import win32api, win32con, win32gui
+import win32con, win32gui
 from PIL import Image, ImageGrab
 
 import time
@@ -11,6 +12,9 @@ import time
 # 防止UI放大导致截图不完整
 # windll.user32.SetProcessDPIAware()
 
+th, tw = 0, 0
+maxLoc = []
+start_pos = []
 def get_window_rect(hwnd):
     try:
         f = ctypes.windll.dwmapi.DwmGetWindowAttribute
@@ -50,29 +54,36 @@ def fetch_image():
     return cv2.cvtColor(np.asarray(grab_image),cv2.COLOR_RGB2BGR),[x1,y1]
 
 
-def click(pos):
+
+
+def click():
+    topLeft = maxLoc
+    # topLeft = minLoc
+    pos = [start_pos[0] + topLeft[0] + tw / 2, start_pos[1] + topLeft[1] + th / 2]
     pyautogui.click(pos[0], pos[1], duration=0.2)
+def isRight(image, template1):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 读取图片，并保留Alpha通道
+    template = cv2.cvtColor(template1, cv2.COLOR_BGR2GRAY)
+    global th, tw
+    th, tw = template.shape[::]
+    # 模板匹配，将alpha作为mask，TM_CCORR_NORMED方法的计算结果范围为[0, 1]，越接近1越匹配
+    result = cv2.matchTemplate(gray, template, cv2.TM_CCORR_NORMED)
+    # result = cv2.matchTemplate(gray, template, cv2.TM_SQDIFF)
+    # 获取结果中最大值和最小值以及他们的坐标
+    global maxLoc
+    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
+    if maxVal < 0.9:
+        return 0
+    else:
+        return 1
 
 
 if __name__ == "__main__":
     import cv2
     image, start_pos = fetch_image()
-    # 转为灰度图
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ih, iw = gray.shape[::]
-    # 读取图片，并保留Alpha通道
-    template = cv2.imread('pic1.png', cv2.IMREAD_UNCHANGED)
-    template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    th, tw = template.shape[::]
-    #模板匹配，将alpha作为mask，TM_CCORR_NORMED方法的计算结果范围为[0, 1]，越接近1越匹配
-    result = cv2.matchTemplate(gray, template, cv2.TM_SQDIFF)
-    # 获取结果中最大值和最小值以及他们的坐标
-    minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(result)
-    topLeft = minLoc
-    bottomRight = (topLeft[0] + tw, topLeft[1] + th)
-    pos = [start_pos[0] + topLeft[0] + tw/2,start_pos[1] + topLeft[1] + th/2]
-    click(pos)
-    # 在窗口截图中匹配位置画红色方框
-    # cv2.rectangle(image, topLeft, bottomRight, 255, 2)
-    # cv2.imshow('Match Template', image)
-    # cv2.waitKey()
+    template = cv2.imread('./img/start_game.png', cv2.IMREAD_UNCHANGED)
+    while isRight(image, template) == 0:
+        time.sleep(1)
+        print("未识别到目标图像")
+    click()
